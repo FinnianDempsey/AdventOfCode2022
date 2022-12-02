461afd4632fa2d8bf58c4be247ccee0439070404
Finnian Dempsey <finnian.dempsey@octopus.com>
2022-12-02 13:46:45 +1000

    updated day1

--- Day1/Day1.sh
+++ Day1/Day1.sh
@@ -10,10 +10,7 @@ while read line || [ -n "$line" ]; do
     if [[ ! -z "$line" ]]; 
     then
         sum=$(($sum + $line))
-        # echo $line
     else 
-        # echo "Sum is $sum"
-
         if [[ $sum -ge $largest_sum ]];
         then
             largest_sum2=$largest_sum
@@ -27,14 +24,11 @@ while read line || [ -n "$line" ]; do
             largest_sum3=$sum
         fi
         sum=0;
-        # echo ' '
     fi;
     
 
 done <$file
-# echo "Sum is $sum"
 
-# echo " "
 echo "Largest sum is $largest_sum"
 
 echo "Largest 3 sums are $largest_sum, $largest_sum2, $largest_sum3 = $(($largest_sum + $largest_sum2 + $largest_sum3))"
